8fe62a04b8439daf90092af1a9e971f744ae0daf
Jonathan Lee <Jonathan.Kaishan@gmail.com>
2026-07-11 14:27:11 +0800

    Rename the SwiftPM package to noctisplanet-foundation

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "npfoundation",
+    name: "noctisplanet-foundation",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
